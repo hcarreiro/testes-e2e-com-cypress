00d877f5f1c3d6ea9f5abf2beffff0ddfb624498
Hildefonso Carreiro <hildefonso.carreiro@gmail.com>
2022-08-05 16:53:48 -0300

    otimização do login

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,6 +2,13 @@
 
 it('successfully logs in', () => {
     cy.intercept('GET', '**/notes').as('getNotes')
-    cy.login()
-    cy.contains('h1', 'Your Notes').should('be.visible')
+    
+    cy.login(
+        // Como definido na variável tenho que passar agora 3 parametros: user, senha e objeto session
+        Cypress.env('USER_EMAIL'),
+        Cypress.env('USER_PASSWORD'),
+        // como estamos passando o cacheSession false, ele vai entrar no else do comando login pra fazer o login padrão
+        { cacheSession: false} 
+    )
+    cy.wait('@getNotes')
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,14 +9,30 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
     cy.get('#confirmationCode').should('be.visible')
 })
 
+
 // Comando customizado para realização de login
 Cypress.Commands.add('login', (
-    userName = Cypress.env('USER_EMAIL'),
-    password = Cypress.env('USER_PASSWORD')
-) => {  
-    cy.visit('/login')
-    cy.get('#email').type(userName)
-    cy.get('#password').type(password, { log: false })
-    cy.contains('button', 'Login').click()
-    cy.contains('h1', 'Your Notes').should('be.visible')
-})
+    username = Cypress.env('USER_EMAIL'),
+    password = Cypress.env('USER_PASSWORD'),
+    // Passamos um objeto com a propriedade 'cacheSession' com default true
+    { cacheSession = true } = {}
+  ) => {
+    // Temos uma variável 'login' que armazena uma função que executa os passos de login
+    const login = () => {
+      cy.visit('/login')
+      cy.get('#email').type(username)
+      cy.get('#password').type(password, { log: false })
+      cy.contains('button', 'Login').click()
+      cy.contains('h1', 'Your Notes').should('be.visible')
+    }
+  
+    // Se cacheSession igual a true, ou seja, se ele existir
+    if (cacheSession) {
+        // Utilizamos a funcionalidade 'cy.session' que verifica se existe uma sessão aberta,
+        // caso exista a sessão ele recupera, caso não, ele tem a variável login que chama a função de login acima
+        cy.session([username, password], login)
+    } else {
+        // Executa o comando de login
+        login()
+    }
+  })
